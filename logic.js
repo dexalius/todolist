b88751d97b35d373255fa62cf8af7642de7cdbbe
dexalius <dhany.af25@gmail.com>
2023-02-11 12:14:58 +0700

    update file

--- logic.js
+++ logic.js
@@ -3,15 +3,17 @@ const inputUser = document.getElementById("input-user");
 const listGroup = document.querySelector(".list-group");
 
 form.addEventListener("submit", function (event) {
-  listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-content-center "><h3>${inputUser.value} </h3><span class="fs-4"><i class="fa-solid fa-trash" id="delete-btn"></i>  <i class="fa-solid fa-circle-check" id"selesai"></i></span></li>`
+  listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-content-center "><h3>${inputUser.value} </h3><span class="fs-4"><i class="fa-solid fa-trash" id="delete-btn"></i>  <i class="fa-solid fa-circle-check" id="selesai"></i></span></li>`
 
   inputUser.value = "";
   event.preventDefault();
 });
 
 listGroup.addEventListener("click", function (event) {
-  if (event.target.id == "delete-btn"){
+if (event.target.id == "delete-btn"){
     event.target.parentElement.parentElement.remove();
+  } else if (event.target.id == "selesai") {
+    event.target.parentElement.previousSibling.classList.toggle("underline");
   }
 
 
